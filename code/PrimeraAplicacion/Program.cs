d8bf3434c775109fa482d06c4e087f16e5aafc8d
pablodarius <pablodarius@hotmail.com>
2020-07-01 22:25:02 +0200

    new videos until 25

--- code/PrimeraAplicacion/Program.cs
+++ code/PrimeraAplicacion/Program.cs
@@ -2,58 +2,15 @@
 /*
 Primer ejemplo de hola mundo
 empezamos!!!
-*/
+
 namespace PrimeraAplicacion
 {
     class Program
     { 
         static void Main(string[] args)
         {
-            /*System.Console.WriteLine("¿Damos vueltas?");
-            string respuesta = Console.ReadLine();
-            while (respuesta != "no") {
-                System.Console.WriteLine("vueltas...");
-                System.Console.WriteLine("Te llamabas...");
-                string nombre = Console.ReadLine();
-                System.Console.WriteLine($"Oye {nombre}, Saldrás del bucle cuando me digas no...");
-                System.Console.WriteLine("¿Seguimos?");
-                respuesta = Console.ReadLine();
-            }
-            System.Console.WriteLine("Acabamos!!!");
 
-            // Adivina un número del 0 al 100 en mayor-menor 
-            Random number = new Random();
-            int n = number.Next(0, 100);
-
-            System.Console.WriteLine("Guess the Number...");
-            int userNum = Int32.Parse(Console.ReadLine());
-            while (n != userNum) {
-                System.Console.WriteLine("Wrong!");
-                if (userNum > n) {
-                    System.Console.WriteLine("Less please, try again...");
-                } else {
-                    System.Console.WriteLine("More please, try again...");
-                }
-
-                System.Console.WriteLine("Guess the Number...");
-                userNum = Int32.Parse(Console.ReadLine());
-            }
-            System.Console.WriteLine($"Yaaaaaaaaaaaaassssssss, well done! the number was {n}");*/
-
-            // Adivina un número del 0 al 100 en mayor-menor 
-            Random number = new Random();
-            int n = number.Next(0, 100);
-            int attempts = 0;
-            int userNum;
-            do {
-                System.Console.WriteLine("Guess the Number...");
-                userNum = Int32.Parse(Console.ReadLine());
-                attempts++;
-
-                if (userNum > n) System.Console.WriteLine("Less please, try again...");
-                if (userNum < n) System.Console.WriteLine("More please, try again...");
-            } while (n != userNum);
-            System.Console.WriteLine($"Yaaaaaaaaaaaaassssssss, well done! the number was {n} and you have needed {attempts} attemps");
-        }        
+        }
+            
     }
-}
+}*/
---
+++ code/PrimeraAplicacion/videos 19 a 24.cs
@@ -0,0 +1,80 @@
+﻿using System;
+/*
+Primer ejemplo de hola mundo
+empezamos!!!
+*/
+namespace PrimeraAplicacion
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            /*System.Console.WriteLine("¿Damos vueltas?");
+            string respuesta = Console.ReadLine();
+            while (respuesta != "no") {
+                System.Console.WriteLine("vueltas...");
+                System.Console.WriteLine("Te llamabas...");
+                string nombre = Console.ReadLine();
+                System.Console.WriteLine($"Oye {nombre}, Saldrás del bucle cuando me digas no...");
+                System.Console.WriteLine("¿Seguimos?");
+                respuesta = Console.ReadLine();
+            }
+            System.Console.WriteLine("Acabamos!!!");
+
+            // Adivina un número del 0 al 100 en mayor-menor 
+            Random number = new Random();
+            int n = number.Next(0, 100);
+
+            System.Console.WriteLine("Guess the Number...");
+            int userNum = Int32.Parse(Console.ReadLine());
+            while (n != userNum) {
+                System.Console.WriteLine("Wrong!");
+                if (userNum > n) {
+                    System.Console.WriteLine("Less please, try again...");
+                } else {
+                    System.Console.WriteLine("More please, try again...");
+                }
+
+                System.Console.WriteLine("Guess the Number...");
+                userNum = Int32.Parse(Console.ReadLine());
+            }
+            System.Console.WriteLine($"Yaaaaaaaaaaaaassssssss, well done! the number was {n}");*/
+
+            // Adivina un número del 0 al 100 en mayor-menor 
+            Random number = new Random();
+            int n = number.Next(0, 100);
+            int attempts = 0;
+            int userNum = 0;
+            do {
+                System.Console.WriteLine("Guess the Number...");
+
+                try {
+                    userNum = Int32.Parse(Console.ReadLine());
+                    attempts++;
+                    if (userNum > n) System.Console.WriteLine("Less please, try again...");
+                    if (userNum < n) System.Console.WriteLine("More please, try again...");
+                /*} catch(FormatException ex) { // se pueden poner ANTES de la general, los catch concretos que queramos tratar de manera especial
+                    System.Console.WriteLine("No has introducido un valor numérico válido");
+                } catch(OverflowException ex) { 
+                    System.Console.WriteLine("Has introducido un valor demasiado grande"); 
+                } catch (Exception ex) { // para recoger la general exception, podría solo ponerse catch {}, pero es mala practica
+                    System.Console.WriteLine("No has introducido un valor válido: " + ex.Message);
+                }*/
+                // para excepcionies con filtro si queremos un tratamiento especial a un, se pone la general con el when
+                } catch (Exception ex) when (ex.GetType() != typeof(FormatException)) { //catch para todas menos la FormatException
+                    System.Console.WriteLine("No has introducido un valor válido: " + ex.Message);
+                } catch (FormatException ex) {
+                    System.Console.WriteLine("No has introducido un valor numérico válido");
+                }
+            } while (n != userNum);
+            System.Console.WriteLine($"Yaaaaaaaaaaaaassssssss, well done! the number was {n} and you have needed {attempts} attemps");
+            System.Console.WriteLine("A partir de esta línea de código el programa continuaría");
+
+            checked { //sin esto, el desbordamiento aritmetico, el compilador lo maneja y devuelve un valor negativo grande como valor erroneo
+                int numero = int.MaxValue;
+                numero += 20;
+                System.Console.WriteLine(numero);
+            } //se puede poner en el IDE que el checked esta aticado sin ponerlo nosotros para todo lo aritmetico
+        }        
+    }
+}
