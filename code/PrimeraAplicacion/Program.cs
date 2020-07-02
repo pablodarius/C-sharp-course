0a7157b5df19a29e04a365b0e64f7aedf3d1aee0
pablodarius <pablodarius@hotmail.com>
2020-07-02 19:12:27 +0200

    finish exceptions

--- code/PrimeraAplicacion/Program.cs
+++ code/PrimeraAplicacion/Program.cs
@@ -2,15 +2,74 @@
 /*
 Primer ejemplo de hola mundo
 empezamos!!!
-
+*/
 namespace PrimeraAplicacion
 {
     class Program
     { 
-        static void Main(string[] args)
+        static void Main(string[] args)        
         {
+            System.IO.StreamReader archivo = null;
 
+            System.Console.WriteLine("Introduce número de mes: ");
+            int numMes = int.Parse(Console.ReadLine());
+            try {
+                System.Console.WriteLine(NombreDelMes(numMes));
+            } catch (Exception e) {
+                System.Console.WriteLine($"Excepción lanzada {e}");
+            }           
+            
+            System.Console.WriteLine("Aquí continua el programa .......");
+
+            try {
+                string linea;
+                int contador = 0;           
+                string path = @"D:\Cursos\Curso c sharp\mes.txt";
+                archivo = new System.IO.StreamReader(path);
+                while ((linea = archivo.ReadLine()) != null) {
+                    System.Console.WriteLine(linea);
+                    contador++;
+                }
+            } catch(Exception e) {
+                System.Console.WriteLine($"Error al leer: {e}");
+            } finally {
+                if (archivo != null) {
+                    archivo.Close();
+                    System.Console.WriteLine("Conexión cerrada!");
+                }
+            }
+        }
+
+        public static string NombreDelMes(int mes) {
+            switch (mes) {
+                case 1:
+                    return "Enero";
+                case 2:
+                    return "Febrero";
+                case 3:
+                    return "Marzo";
+                case 4:
+                    return "Abril";
+                case 5:
+                    return "Mayo";
+                case 6:
+                    return "Junio";
+                case 7:
+                    return "Julio";
+                case 8:
+                    return "Agosto";
+                case 9:
+                    return "Septiembre";
+                case 10:
+                    return "Octubre";
+                case 11:
+                    return "Noviembre";
+                case 12:
+                    return "Diciembre";
+                default:
+                    throw new ArgumentOutOfRangeException(); //toda la info en la bliblioteca de clases .net framework library en google
+            }
         }
             
     }
-}*/
+}
--- code/PrimeraAplicacion/videos 19 a 24.cs
+++ code/PrimeraAplicacion/videos 19 a 24.cs
@@ -2,7 +2,7 @@
 /*
 Primer ejemplo de hola mundo
 empezamos!!!
-*/
+
 namespace PrimeraAplicacion
 {
     class Program
@@ -38,7 +38,7 @@ namespace PrimeraAplicacion
                 System.Console.WriteLine("Guess the Number...");
                 userNum = Int32.Parse(Console.ReadLine());
             }
-            System.Console.WriteLine($"Yaaaaaaaaaaaaassssssss, well done! the number was {n}");*/
+            System.Console.WriteLine($"Yaaaaaaaaaaaaassssssss, well done! the number was {n}");
 
             // Adivina un número del 0 al 100 en mayor-menor 
             Random number = new Random();
@@ -59,7 +59,7 @@ namespace PrimeraAplicacion
                     System.Console.WriteLine("Has introducido un valor demasiado grande"); 
                 } catch (Exception ex) { // para recoger la general exception, podría solo ponerse catch {}, pero es mala practica
                     System.Console.WriteLine("No has introducido un valor válido: " + ex.Message);
-                }*/
+                }
                 // para excepcionies con filtro si queremos un tratamiento especial a un, se pone la general con el when
                 } catch (Exception ex) when (ex.GetType() != typeof(FormatException)) { //catch para todas menos la FormatException
                     System.Console.WriteLine("No has introducido un valor válido: " + ex.Message);
@@ -70,11 +70,14 @@ namespace PrimeraAplicacion
             System.Console.WriteLine($"Yaaaaaaaaaaaaassssssss, well done! the number was {n} and you have needed {attempts} attemps");
             System.Console.WriteLine("A partir de esta línea de código el programa continuaría");
 
-            checked { //sin esto, el desbordamiento aritmetico, el compilador lo maneja y devuelve un valor negativo grande como valor erroneo
-                int numero = int.MaxValue;
-                numero += 20;
-                System.Console.WriteLine(numero);
-            } //se puede poner en el IDE que el checked esta aticado sin ponerlo nosotros para todo lo aritmetico
+            checked { //sin esto, el desbordamiento aritmetico, el compilador lo maneja y devuelve un valor negativo grande como valor erroneo, solo para INT y LONG
+                int num = int.MaxValue;
+                num += 20; // int resultado = unchecked(numero += 20); para que justo esa línea no vaya checkeada en este bloque checked
+                System.Console.WriteLine(num);
+            } //se puede poner en el IDE que el checked esta activado sin ponerlo nosotros para todo lo aritmetico
+            //se puede hacer de forma abreviada sobre la línea que necesita el checked
+            int numero = int.MaxValue;
+            int resultado = checked(numero += 20);
         }        
     }
-}
+}*/
